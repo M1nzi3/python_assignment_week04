7bf00d9f1a01f5a5aadf21e20b588cc378f3b6dd
unknown <miratemsoonthorn@gmail.com>
2024-07-23 13:06:04 +0700

    last update

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def main(num1,num2):
+    print(add(num1,num2))
+    print(subtract(num1,num2))
+    print(multiply(num1,num2))
+    print(divide(num1,num2))
+
+main(10,2)
---
+++ string_operations.py
@@ -0,0 +1,14 @@
+
+def reverse_string(sample_string):
+    reverse_string = sample_string[::-1]
+    return reverse_string
+
+def capitalize_string(sample_string):
+    return sample_string.capitalize()
+
+def lowercase_string(sample_string):
+    return sample_string.lower()
+
+def uppercase_string(sample_string):
+    return sample_string.upper()
+
